8014d0a56a23da92e6199bf7c680732c17904fd1
Sarah Alibay <41892321+sarah1893@users.noreply.github.com>
2023-05-26 16:08:00 +0000

    Import modules for shell component (Doing)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from "@angular/material/button";
+import { MatButtonModule } from '@angular/material/button';
 import { ShellComponent } from './shell/shell.component';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { LayoutModule } from "@angular/cdk/layout";
+import { MatListModule } from "@angular/material/list";
+import { MatMenuModule } from "@angular/material/menu";
 
-
-const modules = [CommonModule, MatButtonModule]
-const components = [ShellComponent]
+const modules = [CommonModule, 
+                 MatButtonModule, 
+                 MatSidenavModule, 
+                 MatToolbarModule,
+                 LayoutModule,
+                 MatMenuModule,
+                 MatListModule,
+                 MatIconModule];
+const components = [ShellComponent];
 
 @NgModule({
   declarations: [...components],
-  imports: [
-    ...modules
-  ],
-  exports: [...components, ...modules]
+  imports: [...modules],
+  exports: [...components, ...modules],
 })
+
 export class SharedModule { }
